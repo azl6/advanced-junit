ea6a067b0e740d78541b4dfdfacda720290a7501
Alex <alex.rodrigues23997@gmail.com>
2022-05-21 22:38:15 -0300

    Parameterized tests with custom arguments source

---
+++ src/main/java/guru/springframework/sfgpetclinic/CustomArgsProvider.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
+
+public class CustomArgsProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of("FL", 7, 10),
+                Arguments.of("OH", 11, 42),
+                Arguments.of("MI", 44, 77));
+    }
+
+    @DisplayName("Custom Provider Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @ArgumentsSource(CustomArgsProvider.class)
+    void fromCustomProviderTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+}
